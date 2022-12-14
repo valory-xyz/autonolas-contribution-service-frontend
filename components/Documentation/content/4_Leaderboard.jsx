e356fc1b9bc09e33a9c96e9a41eecfc51859d5f5
mohandast52 <mohandast52@gmail.com>
2022-12-14 17:19:06 +0530

    docs added

--- components/Documentation/content/4_Leaderboard.jsx
+++ components/Documentation/content/4_Leaderboard.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { Typography } from 'antd/lib';
 import { DOCS_SECTIONS } from '../helpers';
 
@@ -6,7 +7,16 @@ const { Title, Paragraph } = Typography;
 const Leaderboard = () => (
   <div id={DOCS_SECTIONS.leaderboard}>
     <Title level={2}>Leaderboard</Title>
-    <Paragraph>Coming soon</Paragraph>
+    <Paragraph>
+      The Autonolas Contribute leaderboard ranks each community member according
+      to the number of points they have earned.
+    </Paragraph>
+
+    <Paragraph>
+      To earn more points, complete more&nbsp;
+      <Link href={`/docs#${DOCS_SECTIONS.actions}`}>actions</Link>
+      .
+    </Paragraph>
 
     <br />
     <br />
