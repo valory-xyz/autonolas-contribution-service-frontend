f73e95ef89eb2735fe51968be2048d9f843edb18
mohandast52 <mohandast52@gmail.com>
2023-03-29 16:25:22 +0530

    filter out wallet_address that are null

--- common-util/api/leaderboard.js
+++ common-util/api/leaderboard.js
@@ -10,7 +10,7 @@ export const getLeaderboardList = async () => {
   const response = await TileDocument.load(ceramic, process.env.NEXT_PUBLIC_STREAM_ID);
   const users = get(response, 'content.users') || [];
 
-  const usersList = users.map((user, index) => ({
+  const usersList = users.filter((e) => !e.wallet_address).map((user, index) => ({
     ...user,
     // adding a unique key to each user
     rowKeyUi: `${user.twitter_handle}-${user.discord_id}-${index}`,
