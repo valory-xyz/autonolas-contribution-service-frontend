d1b7acd5c24d4472f14bf7ec056c4f7c15cb89ee
Oaksprout <oaksproutthetan@gmail.com>
2023-07-03 13:56:08 +0100

    update docs

--- components/Documentation/content/2_Actions.jsx
+++ components/Documentation/content/2_Actions.jsx
@@ -1,7 +1,7 @@
 import { Typography } from 'antd/lib';
 import { DOCS_SECTIONS } from '../helpers';
 
-const { Title, Paragraph, Text } = Typography;
+const { Title, Paragraph } = Typography;
 
 const ActionsDocs = () => (
   <div id={DOCS_SECTIONS.actions}>
@@ -13,50 +13,9 @@ const ActionsDocs = () => (
     </Paragraph>
 
     <Paragraph>
-      Each action has a name, instructions and points. Take a simple example:
-    </Paragraph>
-
-    <Paragraph>
-      <ul>
-        <li>
-          <Text strong>Name</Text>
-          &nbsp;– Complete an Academy Task
-        </li>
-        <li>
-          <Text strong>Instructions</Text>
-          &nbsp;– Finish one of the tasks between modules and send a screenshot
-          using the&nbsp;
-          <a
-            href="https://forms.gle/vuVNa5823CUMr4Ho6"
-            target="_blank"
-            rel="noreferrer"
-          >
-            claim form
-          </a>
-          .
-        </li>
-        <li>
-          <Text strong>Points</Text>
-          &nbsp;– 4500
-        </li>
-      </ul>
-    </Paragraph>
-
-    <Paragraph>
-      In the action in this example will earn a community member 4500 points.
-    </Paragraph>
-
-    <Paragraph>
-      Actions change regularly – new ones are added, some are removed, points
-      and instructions change etc. See the current set of actions&nbsp;
-      <a
-        href="https://discord.com/channels/899649805582737479/1030087446882418688/1034340826718937159"
-        target="_blank"
-        rel="noreferrer"
-      >
-        here
-      </a>
-      .
+      Each action has a name, instructions and points. When you complete an
+      action, it is automatically tracked and the corresponding points are added
+      to your profile.
     </Paragraph>
     <br />
     <br />
