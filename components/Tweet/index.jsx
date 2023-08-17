c7ea32c40b93500bfc01740ca0dedb041c338dd6
Oaksprout <oaksproutthetan@gmail.com>
2023-08-17 13:07:15 +0100

    adds veolas threshold for tweet submission

--- components/Tweet/index.jsx
+++ components/Tweet/index.jsx
@@ -8,6 +8,7 @@ import { uuid } from 'uuidv4';
 
 import { EducationTitle } from 'common-util/Education/EducationTitle';
 import Proposals from 'components/Proposals';
+import { fetchVeolasBalance } from 'components/MembersList/requests';
 import { useCentaursFunctionalities } from '../CoOrdinate/Centaur/hooks';
 
 const { Text } = Typography;
@@ -32,7 +33,15 @@ const Tweet = () => {
 
   const handleSubmit = async () => {
     setIsSubmitting(true);
+
     try {
+      const balance = await fetchVeolasBalance({ account });
+      if (Number(balance) < 100000000000000000000000n) {
+        throw new Error(
+          'You must hold at least 100k veOLAS to propose a tweet.',
+        );
+      }
+
       const tweetDetails = {
         request_id: uuid(),
         text: tweet,
@@ -66,7 +75,7 @@ const Tweet = () => {
       setTweet('');
     } catch (error) {
       notification.error({
-        message: `Failed to submit proposal: ${error.message}`,
+        message: `Proposal failed: ${error.message}`,
       });
     } finally {
       setIsSubmitting(false);
