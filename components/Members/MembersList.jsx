ed35fb9fe95eb4db13ba18e7d14af90f4c41e8b5
mohandast52 <mohandast52@gmail.com>
2023-10-03 17:33:24 +0530

    fix issue

--- components/Members/MembersList.jsx
+++ components/Members/MembersList.jsx
@@ -1,20 +1,27 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useSignMessage } from 'wagmi';
 import { useRouter } from 'next/router';
+import { useSignMessage } from 'wagmi';
+import { cloneDeep } from 'lodash';
 import {
-  Table, Typography, Button, notification, Card,
+  Table, Typography, Button, notification, Card, Alert,
 } from 'antd/lib';
 import { MessageOutlined, UserAddOutlined } from '@ant-design/icons';
-import { areAddressesEqual, notifyError } from 'common-util/functions';
+
+import {
+  areAddressesEqual,
+  notifyError,
+  notifySuccess,
+} from 'common-util/functions';
 import TruncatedEthereumLink from 'common-util/TruncatedEthereumLink';
 import { useCentaursFunctionalities } from 'components/CoOrdinate/Centaur/hooks';
-import { cloneDeep } from 'lodash';
 import { fetchVeolasBalance } from './requests';
 
 const { Text } = Typography;
 
 export const MembersList = () => {
   const router = useRouter();
+  const [isJoinLoading, setIsJoinLoading] = useState(false);
   const account = useSelector((state) => state?.setup?.account);
   const {
     membersList,
@@ -48,42 +55,26 @@ export const MembersList = () => {
     // await triggerAction(centaurId, action);
   };
 
-  const {
-    signMessageAsync: signMessage,
-    isError,
-    isLoading: isJoinLoading,
-    isSuccess,
-    data,
-  } = useSignMessage({
+  const { signMessageAsync: signMessage, isError: isJoinError } = useSignMessage({
     message: 'Sign this message to verify ownership over this address.',
-
     onSettled: async (_data, error) => {
-      console.log({
-        data: _data,
-        error,
-      });
       if (error) {
         console.error(error);
         notifyError(
           error.details || 'Some error occured while signing the message',
         );
       } else {
+        notifySuccess('Message signed successfully');
         await addNewMember();
       }
     },
   });
 
-  console.log({
-    isError,
-    isJoinLoading,
-    isSuccess,
-    data,
-  });
-
-  const onJoin = async () => {
+  const checkIfHasVeolas = async () => {
     try {
       const balance = await fetchVeolasBalance({ account });
-      if (Number(balance) === 0) {
+      const hasBalance = Number(balance) > 0;
+      if (!hasBalance) {
         notifyError(
           <>
             You must hold veOLAS to join.&nbsp;
@@ -96,12 +87,26 @@ export const MembersList = () => {
             </a>
           </>,
         );
-      } else {
+      }
+      return hasBalance;
+    } catch (error) {
+      notifyError('Error on checking veOLAS balance');
+      console.error(error);
+      return false;
+    }
+  };
+
+  const onJoin = async () => {
+    try {
+      setIsJoinLoading(true);
+      const hasVeolas = await checkIfHasVeolas();
+      if (hasVeolas) {
         await signMessage();
       }
     } catch (error) {
-      notifyError('Join failed');
       console.error(error);
+    } finally {
+      setIsJoinLoading(false);
     }
   };
 
@@ -159,41 +164,52 @@ export const MembersList = () => {
   const isMemberPresent = membersList.some((member) => areAddressesEqual(member.address, account));
 
   return (
-    <Card
-      title="List"
-      bodyStyle={{ padding: 10 }}
-      extra={[
-        !isMemberPresent && (
+    <>
+      {isJoinError && (
+        <Alert
+          message="Join failed, please refresh the page and try again."
+          type="error"
+          className="mb-12"
+          showIcon
+        />
+      )}
+
+      <Card
+        title="List"
+        bodyStyle={{ padding: 10 }}
+        extra={[
+          !isMemberPresent && (
+            <Button
+              type="primary"
+              className="mr-12"
+              disabled={!account}
+              loading={isJoinLoading}
+              onClick={onJoin}
+            >
+              Join
+            </Button>
+          ),
           <Button
-            type="primary"
-            className="mr-12"
             disabled={!account}
-            loading={isJoinLoading}
-            onClick={onJoin}
+            onClick={() => {
+              notification.success({ message: 'Link copied' });
+              navigator.clipboard.writeText(document.URL);
+            }}
           >
-            Join
-          </Button>
-        ),
-        <Button
-          disabled={!account}
-          onClick={() => {
-            notification.success({ message: 'Link copied' });
-            navigator.clipboard.writeText(document.URL);
-          }}
-        >
-          <UserAddOutlined />
-          &nbsp;Invite
-        </Button>,
-      ]}
-    >
-      <Table
-        columns={columns}
-        dataSource={membersList}
-        pagination={false}
-        rowKey={(record) => record.address}
-        bordered
-      />
-    </Card>
+            <UserAddOutlined />
+            &nbsp;Invite
+          </Button>,
+        ]}
+      >
+        <Table
+          columns={columns}
+          dataSource={membersList}
+          pagination={false}
+          rowKey={(record) => record.address}
+          bordered
+        />
+      </Card>
+    </>
   );
 };
 
