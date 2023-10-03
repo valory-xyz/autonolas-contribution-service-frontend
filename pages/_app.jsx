ea0bd25ca1b400efee729a0b01eae298654ffe4f
mohandast52 <mohandast52@gmail.com>
2023-10-03 18:42:07 +0530

    update packages

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,43 +1,39 @@
-/* eslint-disable jest/require-hook */
-import App from 'next/app';
 import { createWrapper } from 'next-redux-wrapper';
+import { ConfigProvider } from 'antd';
 import PropTypes from 'prop-types';
 
-import { WagmiConfig } from 'wagmi';
-
-import Meta from 'common-util/meta';
-import GlobalStyle from 'components/GlobalStyles';
-import Layout from 'components/Layout';
+/** wagmi config */
+import { WagmiConfig as WagmiConfigProvider } from 'wagmi';
 import { wagmiConfig } from 'common-util/Login/config';
+
+/** antd theme config */
+import Layout from 'components/Layout';
+import GlobalStyle from 'components/GlobalStyles';
+import { THEME_CONFIG } from '@autonolas/frontend-library';
+import Meta from 'common-util/meta';
 import initStore from '../store';
 
-require('../styles/antd.less');
+const MyApp = ({ Component, pageProps }) => (
+  <>
+    <GlobalStyle />
+    <Meta />
+    <ConfigProvider theme={THEME_CONFIG}>
+      <WagmiConfigProvider config={wagmiConfig}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </WagmiConfigProvider>
+    </ConfigProvider>
+  </>
+);
 
-class MyApp extends App {
-  static async getInitialProps({ Component, ctx }) {
-    const pageProps = Component.getInitialProps
-      ? await Component.getInitialProps(ctx)
-      : {};
+MyApp.getInitialProps = async ({ Component, ctx }) => {
+  const pageProps = Component.getInitialProps
+    ? await Component.getInitialProps(ctx)
+    : {};
 
-    return { pageProps };
-  }
-
-  render() {
-    const { Component, pageProps } = this.props;
-
-    return (
-      <>
-        <GlobalStyle />
-        <Meta />
-        <WagmiConfig config={wagmiConfig}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </WagmiConfig>
-      </>
-    );
-  }
-}
+  return { pageProps };
+};
 
 MyApp.propTypes = {
   Component: PropTypes.oneOfType([PropTypes.func, PropTypes.shape({})])
