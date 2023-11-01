18f93f290ecfffaeb6163ca4a3d035bee4ba98ff
Oaksprout <oaksproutthetan@gmail.com>
2023-11-01 14:31:20 +0000

    adds hackathon events & improves location display

--- components/Calendar/EventPage.jsx
+++ components/Calendar/EventPage.jsx
@@ -26,11 +26,32 @@ const EventPage = () => {
       <Card>
         <Title level={2}>{event.title || NA}</Title>
         <Text strong>Time</Text>
-        <p>
-          {moment.unix(event.timestamp).format('h:mm a MMMM D YYYY') || NA}
-        </p>
+        <p>{moment.unix(event.timestamp).format('h:mm a MMMM D YYYY') || NA}</p>
         <Text strong>Location</Text>
-        <p>{event.location || NA}</p>
+        <p>
+          {
+            event.location && (
+              // Check if event location is a URL
+              /^http(s)?:\/\//.test(event.location) ? (
+                <a
+                  href={event.location}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {event.location}
+                  {' '}
+                  ↗
+                </a>
+              ) : (
+                // If event location is not a URL, display it as is
+                event.location
+              )
+            )
+               }
+          {!event.location && (
+            NA
+          )}
+        </p>
         <Text strong>Description</Text>
         <p>{event.description || NA}</p>
       </Card>
