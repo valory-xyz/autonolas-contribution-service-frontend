9aa1c073fdb6af7b0dcf6ec528bdd598c24d157d
Oaksprout <oaksproutthetan@gmail.com>
2023-11-02 14:02:59 +0000

    adds new security headers

--- next.config.js
+++ next.config.js
@@ -10,6 +10,35 @@ module.exports = {
       return config;
     },
   }),
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'none';",
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains',
+          },
+        ],
+      },
+    ];
+  },
   publicRuntimeConfig: {},
   images: {
     domains: ['github.com'],
