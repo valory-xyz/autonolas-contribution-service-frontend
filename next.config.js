7ad937d6dc45a19c1749268358daed9d718a97a3
Oaksprout <66292936+oaksprout@users.noreply.github.com>
2023-11-14 07:32:43 +0000

    Adds new security headers (#101)
    * adds new security headers
    
    * refine csp
    
    * reduces csp
    
    * further reduce headers

--- next.config.js
+++ next.config.js
@@ -10,7 +10,32 @@ module.exports = {
       return config;
     },
   }),
-  publicRuntimeConfig: {},
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'none';",
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains',
+          },
+        ],
+      },
+    ];
+  },
+  publicRuntimeConfig: { },
   images: {
     domains: ['github.com'],
     remotePatterns: [
