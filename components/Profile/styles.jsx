85a62e54e9333be65a610a4f894f6751e1c42ae8
mohandast52 <mohandast52@gmail.com>
2023-11-23 00:35:00 +0530

    fix

--- components/Profile/styles.jsx
+++ components/Profile/styles.jsx
@@ -4,6 +4,7 @@ import { Card } from 'antd';
 export const IMAGE_SIZE = 250;
 
 export const BadgeCard = styled(Card)`
-  width: ${IMAGE_SIZE}px;
-  height: ${IMAGE_SIZE}px;
+  .ant-card-body {
+    padding: 0;
+  }
 `;
