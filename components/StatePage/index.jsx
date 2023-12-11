0083142d424ff2bb6769262e716f178b3024608c
Oaksprout <oaksproutthetan@gmail.com>
2023-12-11 16:21:28 +0000

    Add page to view and debug state

---
+++ components/StatePage/index.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import styled from 'styled-components';
+
+import { useCentaursFunctionalities } from 'components/CoOrdinate/Centaur/hooks';
+
+const StyledPre = styled.pre`
+  max-width: 800px;
+  overflow-x: auto;
+`;
+
+const StatePage = () => {
+  const { currentMemoryDetails, fetchedUpdatedMemory } = useCentaursFunctionalities();
+
+  useEffect(() => {
+    fetchedUpdatedMemory();
+  });
+
+  return (
+    <StyledPre>{JSON.stringify(currentMemoryDetails, undefined, 2)}</StyledPre>
+  );
+};
+
+export default StatePage;
