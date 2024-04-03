a6afac1bf78ad382181151378583766b13fadcbc
Atatakai <atatakainyan@gmail.com>
2024-04-03 18:36:21 +0400

    Add file validation

--- components/Tweet/UploadButton.jsx
+++ components/Tweet/UploadButton.jsx
@@ -4,10 +4,14 @@ import PropTypes from 'prop-types';
 import { FileImageOutlined } from '@ant-design/icons';
 import { uploadToIpfs } from './utils';
 
+const SUPPORTED_FILE_TYPES = ['image/jpeg', 'image/png'];
+const MAX_IMAGE_SIZE_IN_MB = 5;
+const MAX_IMAGE_SIZE = MAX_IMAGE_SIZE_IN_MB * 1024 * 1024;
+const ACCEPT = SUPPORTED_FILE_TYPES.join(',');
+
 const UploadButton = ({
   disabled,
   title,
-  accept,
   onUploadMedia,
   ...rest
 }) => {
@@ -18,6 +22,18 @@ const UploadButton = ({
     try {
       const file = e.target.files[0];
       if (!file) return;
+
+      // Validate file type
+      if (!SUPPORTED_FILE_TYPES.contains(file.type)) {
+        message.error('Unsupported file type. Please select an image file.');
+        return;
+      }
+
+      // Validate file size
+      if (file.size > MAX_IMAGE_SIZE) {
+        message.error(`File size limit is ${MAX_IMAGE_SIZE_IN_MB} MB. Please select a smaller file.`);
+        return;
+      }
 
       setUploading(true);
 
@@ -43,7 +59,7 @@ const UploadButton = ({
     <>
       <input
         type="file"
-        accept={accept}
+        accept={ACCEPT}
         onChange={handleFileInputChange}
         style={{ display: 'none' }}
         ref={inputRef}
@@ -65,14 +81,12 @@ const UploadButton = ({
 UploadButton.propTypes = {
   disabled: PropTypes.bool,
   title: PropTypes.string,
-  accept: PropTypes.string,
   onUploadMedia: PropTypes.func,
 };
 
 UploadButton.defaultProps = {
   disabled: true,
   title: 'Add Image',
-  accept: 'image/jpeg,image/png',
   onUploadMedia: null,
 };
 
