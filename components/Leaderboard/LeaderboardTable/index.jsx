8860e61c10b0bf3ac145b68594d5d5a8bf3de0fd
mohandast52 <mohandast52@gmail.com>
2024-06-12 18:09:37 +0530

    chore: Update LeaderboardTable to adjust width of Socials column

--- components/Leaderboard/LeaderboardTable/index.jsx
+++ components/Leaderboard/LeaderboardTable/index.jsx
@@ -40,52 +40,52 @@ const Leaderboard = () => {
     },
     {
       title: 'Socials',
-      width: 140,
+      width: 120,
       render: (record) => {
         const { wallet_address, twitter_handle, discord_id, rowKeyUi } = record;
 
         const socials = [
           wallet_address && (
-            <span className="mr-12">
-              <a
-                href={`https://etherscan.io/address/${wallet_address}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Wallet address"
+            <a
+              href={`https://etherscan.io/address/${wallet_address}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Wallet address"
+              key={`${rowKeyUi}-wallet`}
+              className="mr-12"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 320 512"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="1em"
-                  viewBox="0 0 320 512"
-                >
-                  <path
-                    fill={COLOR.GREY_2}
-                    d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"
-                  />
-                </svg>
-              </a>
-            </span>
+                <path
+                  fill={COLOR.GREY_2}
+                  d="M311.9 260.8L160 353.6 8 260.8 160 0l151.9 260.8zM160 383.4L8 290.6 160 512l152-221.4-152 92.8z"
+                />
+              </svg>
+            </a>
           ),
           twitter_handle && (
-            <span className="mr-12">
-              <a
-                href={`https://twitter.com/${twitter_handle}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="Twitter handle"
+            <a
+              href={`https://twitter.com/${twitter_handle}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Twitter handle"
+              key={`${rowKeyUi}-twitter`}
+              className="mr-12"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="1em"
+                viewBox="0 0 512 512"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  height="1em"
-                  viewBox="0 0 512 512"
-                >
-                  <path
-                    fill={COLOR.GREY_2}
-                    d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
-                  />
-                </svg>
-              </a>
-            </span>
+                <path
+                  fill={COLOR.GREY_2}
+                  d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
+                />
+              </svg>
+            </a>
           ),
           discord_id && (
             <a
@@ -93,6 +93,7 @@ const Leaderboard = () => {
               target="_blank"
               rel="noopener noreferrer"
               aria-label="Discord ID"
+              key={`${rowKeyUi}-discord`}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -112,7 +113,7 @@ const Leaderboard = () => {
 
         return socials.map((social, index) => (
           <Text type="secondary" key={`${rowKeyUi}-social-${index}`}>
-            {social}{' '}
+            {social}
           </Text>
         ));
       },
