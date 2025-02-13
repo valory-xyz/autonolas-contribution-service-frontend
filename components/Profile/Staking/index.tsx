76398f726cae22b957b58cca1e6d2be78d2c3a6e
Atatakai <densatsu@mail.ru>
2025-02-13 20:05:40 +0400

    feat: add locked users modal

--- components/Profile/Staking/index.tsx
+++ components/Profile/Staking/index.tsx
@@ -1,7 +1,6 @@
 import { Button, Card, Flex, Typography } from 'antd';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { XProfile } from 'types/x';
