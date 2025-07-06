6e1d1d95b4ead5a65ee0a2b984b8f817b54b2ffc
Rajat Verma <rajatverma5885045@gmail.com>
2025-07-06 18:05:51 +0530

    fix: remove chat page

--- pages/chat/index.jsx
+++
@@ -1,6 +0,0 @@
-// import Chat from 'components/Chat';
-// export default Chat;
-// TODO: It will be added once the page is updated.
-import { PageDoesNotExist } from 'common-util/PageDoesNotExist';
-
-export default PageDoesNotExist;
