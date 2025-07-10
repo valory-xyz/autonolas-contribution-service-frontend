2fc97df7b2ef193ac9594d9f1949bded93e30330
Rajat Verma <rajatverma5885045@gmail.com>
2025-07-10 23:21:43 +0530

    fix: dedupe

--- pages/api/agent-staking.ts
+++ pages/api/agent-staking.ts
@@ -1,6 +1,6 @@
-import { Wallet } from 'ethers';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+import { getSignature } from 'common-util/apiRoute';
 import { getNowTimestamp } from 'common-util/functions/time';
 import { ContributeAgent } from 'types/users';
 
@@ -49,11 +49,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const agent: ContributeAgent = await agentResponse.json();
 
     // Create a signature for updating the data
-    const privateKey = process.env.AGENT_DB_WALLET_PRIVATE_KEY;
-    if (!privateKey) throw new Error('Missing AGENT_DB_WALLET_PRIVATE_KEY');
-    const wallet = new Wallet(privateKey);
     const message = `timestamp:${getNowTimestamp()},endpoint:${ENDPOINT_URL}/${attributeId}`;
-    const signature = await wallet.signMessage(message);
+    const signature = await getSignature(message);
 
     // Send update to database
     const response = await fetch(`${BASE_URL}/${attributeId}`, {
